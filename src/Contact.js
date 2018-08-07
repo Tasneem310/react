ea0edfe7aae596946e102e86137eb7bcd0e1d460
Tasneem310 <41957155+Tasneem310@users.noreply.github.com>
2018-08-07 07:28:03 +0300

    add files in react

---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+	  
+        <section class="sections">
+  <h1>Contact</h1>
+  <p>My Address: Riyadh- Saudi Arabia </p>
+  <p>My Email: Tasneem.m310@gmail.com </p>
+  <p>My pnone No.: 0505555555</p>
+</section> 
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
---
+++ src/Home.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <section class="sections">
+  <h1>Tasneem Almurad</h1>
+  <p>Someone who is interested in technology and everything that is new. I like poll and reading in different fields. </p>
+</section>
+<section class="sections">
+  <h1>Education</h1>
+  <p>Graduate from ALQassim University - Computer College - IT Dept. </p>
+</section>
+<section class="sections">
+  <h1>Skills</h1>
+  <h3>Software</h3>
+  <p>Microsoft Office,<span> Linux,</span> photofiltre studio x,<span> Filmora,</span> Proshow Producer</p>
+  <h3>Design</h3>
+  <p>Web Application</p>
+  <h3>Programming Language</h3>
+  <p>Java,<span> html5,</span> css3,<span> Java Script,</span> C++ </p>
+</section>
+<section class="sections">
+  <h1>Accounts</h1>
+  <p>Twitter: <a href ="http://www.twitter.com/meme__310">Meme__310</a> </p>
+  <p>Youtube Chanel: <a href ="https://www.youtube.com/channel/UCzmXxuR7lElz0S0CbvUOVDA?view_as=subscriber">Tasneem M</a> </p>
+  <p>Instagram: <a href ="https://www.instagram.com/Meme__310">Meme__310</a> </p>
+</section>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
